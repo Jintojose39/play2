eac62624501661a46d676c5b6285ac7ad1efe443
Jinto Jose <jinto.jose@testingmavens.com>
2025-04-07 15:01:47 +0530

    code added for playwright learn

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,11 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './tests-examples',
+  timeout : 40*1000,
+  expect:{
+    timeout :40*1000,
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -31,24 +35,26 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless : false,
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+       }
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,11 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test("Page playwright test", async ({ page }) => {
+test.only("Page playwright test", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.locator(`//*[@id="username"]`).fill('rahulshettyacademy');
+  await page.locator(`//*[@id="password"]`).fill('wrong');
+  await page.locator(`#signInBtn`).click();
+  console.log(await page.locator('[style*="block"]').textContent());
+  await expect(page.locator('[style*="block"]')).toContainText('Incorrect username/password.');
 });
 
 test("Page playwright with browser", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.goto("https://google.com");
+  // Expect a title "to contain" a substring.
+  let pageTitle = await page.title();
+  await expect(page).toHaveTitle(pageTitle);
 });
